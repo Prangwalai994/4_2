4ea4000fe84153e740d50f21e016cef11091570d
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 22:35:21 +0700

    4_2 Modify 1

---
+++ FizzBuzzUnitTest4_2/UnitTest1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using _20_Solution_FizzBuzz4_2.Fundamentals;
+using NUnit.Framework;
+
+namespace FizzBuzzUnitTest4_2
+{
+
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+        [Test]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+        [Test]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber()
+        {
+            var result = FizzBuzz.GetOutput(3);
+
+            Assert.That(result, Is.EqualTo("1"));
+        }
+    }
+        
+}
